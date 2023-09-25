62cb9b677db82b4b444790c311aa5d873de347af
unknown <sarabayrami01@gmail.com>
2023-09-25 19:20:49 +0330

    Phonebook program without SQL Server

---
+++ Phonebook/Phonebook/Book.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Phonebook
+{
+    public class Book
+    {
+        private Contact[] contacts=new Contact[20];
+
+        public int TotalContact
+        {
+            get
+            {
+                int totalContacts = 0;
+                for (int i = 0; i < contacts.Length; i++)
+                {
+                    if (contacts[i] != null)
+                        totalContacts++;
+                }
+                return totalContacts;
+            }
+        }
+
+        public void Add(Contact contact)
+        {
+            for (int i = 0; i < contacts.Length; i++) 
+            {
+                if (contacts[i] == null)
+                {
+                    contacts[i] = contact;
+                    break;
+                }
+            }
+        }
+
+        public Contact CreateContact() 
+        {
+            Contact newContact = new Contact();
+            Console.Write("Name:");
+            newContact.Name = Console.ReadLine();
+            Console.Write("Phone Number: ");
+            newContact.PhoneNumber = Console.ReadLine();
+            Console.Write("Email:");
+            newContact.Email = Console.ReadLine();
+            Console.Write("Address:");
+            newContact.Address = Console.ReadLine();
+            return newContact;
+        }
+
+        public void UpdateContact(string number, Contact contact)
+        {
+            var oldContact = FindByNumber(number);
+            oldContact.Name= contact.Name;
+            oldContact.Email= contact.Email;
+            oldContact.PhoneNumber= contact.PhoneNumber;
+            oldContact.Address=contact.Address;
+        }
+
+        public void Remove(string number)
+        {
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                if (contacts[i]!=null && contacts[i].PhoneNumber == number)
+                    contacts[i] = null;
+            }
+        }
+
+        public Contact FindByName(string name)
+        {
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                if (contacts[i] != null && contacts[i].Name == name)
+                    return contacts[i];
+            }
+            return null;
+        }
+
+        public Contact FindByNumber(string number)
+        {
+            for (int i = 0; i < contacts.Length; i++)
+            {
+                if (contacts[i] != null && contacts[i].PhoneNumber == number)
+                    return contacts[i];
+            }
+            return null;
+        }
+    }
+}
---
+++ Phonebook/Phonebook/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Phonebook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Phonebook (total contact " + book.TotalContact + ")");
+                DisplayMenu();
+                Console.Write("Please select an option: ");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        var newContact=book.CreateContact();
+                        book.Add(newContact);
+                        break;
+                    case "2":
+                        Console.Write("Please enter phone number to update: ");
+                        string phoneNumberToUpdate = Console.ReadLine();
+                        Contact contactToUpdate = book.FindByNumber(phoneNumberToUpdate);
+                        if (contactToUpdate != null)
+                        {
+                            var updatedContact = book.CreateContact();
+                            book.UpdateContact(phoneNumberToUpdate,updatedContact);
+                        }
+                        else
+                            Console.WriteLine("Contact with phone" + phoneNumberToUpdate + "was not found");
+                        break;
+                    case "3":
+                        Console.WriteLine("Please enter phone number to remove: ");
+                        string phoneToRemove=Console.ReadLine();
+                        book.Remove(phoneToRemove);
+                        break;
+                    case "4":
+                        Console.Write("Please enter name to search: ");
+                        string nameToSearch = Console.ReadLine();
+                        Contact contactByName = book.FindByName(nameToSearch);
+                        if (contactByName != null)
+                        {
+                            DisplayContactInformation(contactByName);
+                        }
+                        else
+                            Console.WriteLine("Contact with name" + contactByName + "was not found");
+                        Console.ReadKey();
+                        break;
+                    case "5":
+                        Console.Write("Please enter phone number to search: ");
+                        string phoneNumberToSearch=Console.ReadLine();
+                        Contact contactByNumber= book.FindByNumber(phoneNumberToSearch);
+                        if(contactByNumber!=null)
+                        {
+                            DisplayContactInformation(contactByNumber);
+                        }
+                        else
+                            Console.WriteLine("Contact with phone"+phoneNumberToSearch+"was not found");
+                        Console.ReadKey();
+                        break;
+                    case "6":
+                        return;
+                        break;
+                }
+            }
+
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine();
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Update Contact");
+            Console.WriteLine("3. Delete Contact");
+            Console.WriteLine("4. Search by Name");
+            Console.WriteLine("5. Search by Number");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine();
+        }
+
+        private static void DisplayContactInformation(Contact contact)
+        {
+            Console.WriteLine("Name:" + contact.Name);
+            Console.WriteLine("Phone Number:" + contact.PhoneNumber);
+            Console.WriteLine("Email:" + contact.Email);
+            Console.WriteLine("Address:" + contact.Address);
+        }
+    }
+}
